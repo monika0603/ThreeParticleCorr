7ce5d244996f94bae360483ab8dfdc35d9075f47
monika0603 <monika.pu@gmail.com>
2015-05-02 22:30:44 -0400

    Update config file for crab jobs

--- TriHadronAnalyzer/PbPbRun2011/crabConfig.py
+++ TriHadronAnalyzer/PbPbRun2011/crabConfig.py
@@ -2,22 +2,22 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.section_('General')
-config.General.requestName = 'AwayAnalysisEtaAsso_0_1_2_Pt3_10'
-config.General.workArea = 'pPbRun2013'
+config.General.requestName = 'Three-particleCorrFirstAttempt'
+config.General.workArea = 'PbPbRun2011'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.section_('JobType')
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'awaySideAnalyzer_PAHighPt_cfg.py'
-config.JobType.outputFiles = ['pPb_Pbp_CombinedDiHadronCorrFunc_NoCutetaTrg_EtaAsso_0_1_2_Pt3_10.root']
+config.JobType.psetName = 'trihadronanalyzerPbPb_cfg.py'
+config.JobType.outputFiles = ['TriHadronFirstAttempt.root']
 
 config.section_('Data')
-config.Data.inputDataset = '/PAHighPt/HIRun2013-PromptReco-v1/RECO'
+config.Data.inputDataset = '/HIMinBiasUPC/HIRun2011-14Mar2014-v2/RECO'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 20
-config.Data.lumiMask = 'Cert_pPb_Pbpcombined_HI_PromptReco_Collisions13_JSON.txt'
+config.Data.unitsPerJob = 10
+config.Data.lumiMask = 'Cert_181530-183126_HI7TeV_PromptReco_Collisions11_JSON.txt'
 config.Data.publication = False
 #config.Data.runRange = '193093-193999' # '193093-194075'
 #config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
