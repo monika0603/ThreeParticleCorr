b182cfd82985d0b6faa8c4be066e5f50643210a8
monika0603 <monika.pu@gmail.com>
2015-05-22 22:12:57 -0400

    Issue with efficiency numbers in mixed events

--- TriHadronAnalyzer/src/TriHadronAnalyzer.cc
+++ TriHadronAnalyzer/src/TriHadronAnalyzer.cc
@@ -718,7 +718,7 @@ TriHadronAnalyzer::endJob()
                         if(deltaPhi2 > -pi_ && deltaPhi2 < -pi_/2.0) deltaPhi2 = deltaPhi2 + 2*pi_;
                         
                         if(deltaPhi1 == 0 && deltaPhi2 == 0) exit(EXIT_FAILURE);
-                        cout<<eff_f<<'\t'<<eff_s<<'\t'<<1.0/nMult_trg1/eff_f/eff_s<<endl;
+                       // cout<<eff_f<<'\t'<<eff_s<<'\t'<<1.0/nMult_trg1/eff_f/eff_s<<endl;
                         
                         if(ass_fEta_ == 0 && ass_sEta_==0) {
                             hBackground_["0"]->Fill(deltaPhi1,deltaPhi2,1.0/nMult_trg1/eff_f/eff_s); }
